9d9d5ed5625b8b146a818766e4cff21cc15924ef
Jiaxing Shi <quic_jiaxshi@quicinc.com>
2022-11-01 03:20:33 +0000

    meta-ros: scripts: include content for SDK in OE_SKIP_SDK_CHECK
    Env set up for SDK should not be used in full building with src.
    OE_SKIP_SDK_CHECK is defined in SDK enviroment.
    
    Change-Id: I73ba6b44123fcf385d013102497f7dba56ea6c59

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -13,26 +13,28 @@ BBLAYER_CONF=$BUILDDIR/conf/bblayers.conf
 Image_bb=$BUILDDIR/layers/poky/meta-qti-bsp/recipes-products/images/qti-robotics-image.bb
 Install_pkgs="packagegroup-ros2-foxy"
 
-#Add layers to bblayers.conf
-for layer in ${ROS_LAYERS[@]}; do
-    if [[ "$(cat $BBLAYER_CONF)" =~ $layer ]];  then
-        echo "$layer has been added to bblayers.conf"
-    else
-        echo "Add $layer to $BBLAYER_CONF"
-        echo "BBLAYERS += \" \${SDKBASEMETAPATH}/layers/$layer \"" >> $BBLAYER_CONF
-    fi
-done
+if [[ $OE_SKIP_SDK_CHECK =~ "1" ]]; then
+    #Add layers to bblayers.conf
+    for layer in ${ROS_LAYERS[@]}; do
+        if [[ "$(cat $BBLAYER_CONF)" =~ $layer ]];  then
+            echo "$layer has been added to bblayers.conf"
+        else
+            echo "Add $layer to $BBLAYER_CONF"
+            echo "BBLAYERS += \" \${SDKBASEMETAPATH}/layers/$layer \"" >> $BBLAYER_CONF
+        fi
+    done
 
-for pkg in ${Install_pkgs[@]}; do
-    if [[ "$(cat $Image_bb)" =~ $pkg ]];  then
-        echo "$pkg has been added to $Image_bb"
-    else
-        echo "CORE_IMAGE_EXTRA_INSTALL +=  \" $pkg  \"" >> $Image_bb
-    fi
-done
+    for pkg in ${Install_pkgs[@]}; do
+        if [[ "$(cat $Image_bb)" =~ $pkg ]];  then
+            echo "$pkg has been added to $Image_bb"
+        else
+            echo "CORE_IMAGE_EXTRA_INSTALL +=  \" $pkg  \"" >> $Image_bb
+        fi
+    done
 
-if [[ "$(cat $Image_bb)" =~ "inherit qti_gen_incremental_pkg" ]];  then
-	echo "qti_gen_incremental_pkg class has been added"
-else
-    echo "inherit qti_gen_incremental_pkg" >> $Image_bb
+    if [[ "$(cat $Image_bb)" =~ "inherit qti_gen_incremental_pkg" ]];  then
+        echo "qti_gen_incremental_pkg class has been added"
+    else
+        echo "inherit qti_gen_incremental_pkg" >> $Image_bb
+    fi
 fi
